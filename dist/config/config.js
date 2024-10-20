668a9a10d6fb8ebaad13d23c94a0e8ce748b9bec
Daniel Aguirre <main.data06@gmail.com>
2024-10-20 01:32:28 -0300

    .gitignore fix

--- dist/config/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/config");
-exports.default = {
-    MONGO_URI: process.env.MONGODB_URI,
-    PORT: process.env.PORT || 8080,
-};
